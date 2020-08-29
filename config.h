75780bd41150d3d58c3ff494273ba300720760c9
Goldcoders <goldcoders@protonmail.com>
2020-08-29 19:52:57 +0800

    use superscript to indicate tag number

--- config.h
+++ config.h
@@ -39,7 +39,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1:", "2:", "3:", "4:﬏", "5:", "6:", "7:戮", "8:", "9:" };
+static const char *tags[] = { " ¹", " ²", " ³", "﬏ ⁴", " ⁵", " ⁶", "戮⁷", " ⁸", " ⁹" };
 
 static const Rule rules[] = {
 	/* xprop(1):
