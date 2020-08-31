bb69dbccb954a6d31109eec33de4f477e8da1d05
Goldcoders <goldcoders@protonmail.com>
2020-08-31 14:45:37 +0800

    added inner gaps by 20 pixel h and v

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 
 /* appearance */
 static const unsigned int borderpx       = 0;        /* border pixel of windows */
-static const unsigned int snap           = 32;       /* snap pixel */
+static const unsigned int snap           = 20;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 0;   /* systray spacing */
+static const unsigned int systrayspacing = 5;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;     /* 0 means no systray */
-static const unsigned int gappih         = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv         = 0;       /* vert inner gap between windows */
+static const unsigned int gappih         = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv         = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh         = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps               = 0;        /* 1 means no outer gap when there is only one window */
